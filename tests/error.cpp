8703a9d68ddd45e5d92bd242190ac770291637d4
Amanda Wee <amanda@aranel.net>
2020-06-06 00:30:10 +1200

    Add unit tests for type error

--- tests/error.cpp
+++ tests/error.cpp
@@ -24,3 +24,24 @@ TEST_CASE("ConstraintError")
     conn.execute("INSERT INTO person (name) VALUES ('Alice');");
     REQUIRE_THROWS_AS(conn.execute("INSERT INTO person (name) VALUES ('Alice');"), sqlitemm::ConstraintError);
 }
+
+TEST_CASE("TypeError")
+{
+    sqlitemm::Connection conn(":memory:");
+    REQUIRE_NOTHROW(conn.execute("CREATE TABLE person (id INTEGER PRIMARY KEY, name TEXT UNIQUE);"
+                                 "INSERT INTO person (name) VALUES ('Alice');"));
+    auto statement = conn.prepare("SELECT name FROM person");
+    auto result = statement.execute_query(true);
+    result.step();
+    int x;
+
+    SECTION("type error on result field conversion")
+    {
+        REQUIRE_THROWS_AS(x = result[0], sqlitemm::TypeError);
+    }
+
+    SECTION("type error on result conversion")
+    {
+        REQUIRE_THROWS_AS(result >> x, sqlitemm::TypeError);
+    }
+}
